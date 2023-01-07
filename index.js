9821f6becf7024c1c3b025d30eacd3969d4ff9ed
keerthipriya5 <113234960+keerthipriya5@users.noreply.github.com>
2023-01-07 08:49:23 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-let port = 3000;
-if (typeof process.env.PORT !== "undefined") {
-  port = process.env.PORT;
-}
-
-app.listen(port, () => {
-  console.log("Started express server at port " + port);
-});
